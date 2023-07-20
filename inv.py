25400c9b19b24154af91d5277ad534d3f8ba8f45
simmons2714 <67743604+simmons2714@users.noreply.github.com>
2023-07-20 16:04:08 -0400

    Update inv.py

--- inv.py
+++ inv.py
@@ -5,9 +5,6 @@ def add_to_inventory(adding_item):
     with open(filepath, 'r') as fp:
         information = json.load(fp)
 
-    
-    #information.update({'Inventory': test_list})
-    #information.update({'Inventory': []})
     information.setdefault('Inventory', []).append(adding_item)
 
     with open(filepath, 'w') as fp:
