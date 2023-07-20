4e67dee4cd4a0877aef69e104fe3f4a712c811fe
simmons2714 <67743604+simmons2714@users.noreply.github.com>
2023-07-20 16:04:29 -0400

    Update nouns.py

--- nouns.py
+++ nouns.py
@@ -1,6 +1,6 @@
 import json
 
-def add_noun(thing_name, thing_desc):
+def create_noun(thing_name, thing_desc):
     things_dict = {
         thing_name : thing_desc
     }
@@ -11,3 +11,23 @@ def add_noun(thing_name, thing_desc):
     # Writing to sample.json
     with open("nouns.json", "w") as outfile:
         outfile.write(json_object)
+
+def add_noun(thing_name, thing_desc):
+    filepath = 'nouns.json'
+    with open(filepath, 'r') as fp:
+        information = json.load(fp)
+
+    information.update({thing_name:thing_desc})
+
+    with open(filepath, 'w') as fp:
+        json.dump(information, fp, indent=4)
+
+def remove_noun(thing_name):
+    filepath = 'nouns.json'
+    with open(filepath, 'r') as fp:
+        information = json.load(fp)
+
+    information.pop(thing_name)
+
+    with open(filepath, 'w') as fp:
+        json.dump(information, fp, indent=4)
