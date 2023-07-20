cfcd16dfedca0bcfde847338d2659f3a17e3053f
simmons2714 <67743604+simmons2714@users.noreply.github.com>
2023-07-20 16:04:50 -0400

    Update special.py

--- special.py
+++ special.py
@@ -1,179 +1,70 @@
-def assign_stats():
-    special_stat_loop = True
-    inital_stats = 40
-    usr_int = 0
-    strength = 0
-    perception = 0
-    endurance = 0
-    charisma = 0
-    intelligence = 0
-    agility = 0
-    luck = 0
-    while special_stat_loop:
-        if inital_stats <= 0:
-            strength += 0
-            special_stat_loop = False
-            break
-        else:
-            while strength < 11:
-                usr_int = int(input("Strength: "))
-                if usr_int < 0 or usr_int > 10:
-                    print('Please enter a value between -1 and 11')
-                    continue
-                else:
-                    if usr_int > inital_stats and inital_stats == 0:
-                        print('Out of stat points allocating the rest as zero.')
-                        strength += 0
-                        break
-                    elif usr_int > inital_stats and inital_stats > 0:
-                        print(f'Stat points left: {inital_stats}. Please enter a value between zero and {inital_stats}.')
-                        continue
-                    else:
-                        strength += usr_int
-                        inital_stats -= usr_int
-                        break
+import questionary
+from questionary import ValidationError, Validator
 
-        if inital_stats <= 0:
-            perception += 0
-            special_stat_loop = False
-            break
-        else:
-            while perception < 11:
-                usr_int = int(input("Perception: "))
-                if usr_int < 0 or usr_int > 10:
-                    print('Please enter a value between -1 and 11')
-                    continue
-                else:
-                    if usr_int > inital_stats and inital_stats == 0:
-                        print('Out of stat points allocating the rest as zero.')
-                        perception += 0
-                        break
-                    elif usr_int > inital_stats and inital_stats > 0:
-                        print(f'Stat points left: {inital_stats}. Please enter a value between zero and {inital_stats}.')
-                        continue
-                    else:
-                        perception += usr_int
-                        inital_stats -= usr_int
-                        break
+class SpecialValidator(Validator):
+    def __init__(self, special_stats, stat_name):
+        self.special_stats = special_stats
+        self.stat_name = stat_name
 
-        if inital_stats <= 0:
-            endurance += 0
-            special_stat_loop = False
-            break
-        else:
-            while endurance < 11:
-                usr_int = int(input("Endurance: "))
-                if usr_int < 0 or usr_int > 10:
-                    print('Please enter a value between -1 and 11')
-                    continue
-                else:
-                    if usr_int > inital_stats and inital_stats == 0:
-                        print('Out of stat points allocating the rest as zero.')
-                        perception += 0
-                        break
-                    elif usr_int > inital_stats and inital_stats > 0:
-                        print(f'Stat points left: {inital_stats}. Please enter a value between zero and {inital_stats}.')
-                        continue
-                    else:
-                        endurance += usr_int
-                        inital_stats -= usr_int
-                        break
+    def validate(self, document):
+        try:
+            current_points = int(document.text)
+            if not (1 <= current_points <= 10):
+                raise ValidationError(
+                    message="Please enter a valid number between 1 and 10",
+                    cursor_position=0,
+                )
 
-        if inital_stats <= 0:
-            charisma += 0
-            special_stat_loop = False
-            break
-        else:
-            while charisma < 11:
-                usr_int = int(input("Charisma: "))
-                if usr_int < 0 or usr_int > 10:
-                    print('Please enter a value between -1 and 11')
-                    continue
-                else:
-                    if usr_int > inital_stats and inital_stats == 0:
-                        print('Out of stat points allocating the rest as zero.')
-                        charisma += 0
-                        break
-                    elif usr_int > inital_stats and inital_stats > 0:
-                        print(f'Stat points left: {inital_stats}. Please enter a value between zero and {inital_stats}.')
-                        continue
-                    else:
-                        charisma += usr_int
-                        inital_stats -= usr_int
-                        break
+            temp_stats = self.special_stats.copy()
+            temp_stats[self.stat_name] = current_points
+            total_points = sum(temp_stats.values())
+            if total_points > 40:
+                raise ValidationError(
+                    message="Total points allocated cannot exceed 40",
+                    cursor_position=0,
+                )
+        except (ValueError, TypeError):
+            raise ValidationError(
+                message="Please enter a valid number between 1 and 10",
+                cursor_position=0,
+            )
+def get_special_stats():
+    answers = {}
+    special_stats = {
+        "Strength": 1,
+        "Perception": 1,
+        "Endurance": 1,
+        "Charisma": 1,
+        "Intelligence": 1,
+        "Agility": 1,
+        "Luck": 1,
+    }
 
-        if inital_stats <= 0:
-            intelligence += 0
-            special_stat_loop = False
-            break
-        else:
-            while intelligence < 11:
-                usr_int = int(input("Intelligence: "))
-                if usr_int < 0 or usr_int > 10:
-                    print('Please enter a value between -1 and 11')
-                    continue
-                else:
-                    if usr_int > inital_stats and inital_stats == 0:
-                        print('Out of stat points allocating the rest as zero.')
-                        intelligence += 0
-                        break
-                    elif usr_int > inital_stats and inital_stats > 0:
-                        print(f'Stat points left: {inital_stats}. Please enter a value between zero and {inital_stats}.')
-                        continue
-                    else:
-                        intelligence += usr_int
-                        inital_stats -= usr_int
-                        break
+    # List of stat names in the order they will be asked
+    stat_names = list(special_stats.keys())
+    for stat_name in stat_names:
+        questions = questionary.text(
+            f'{stat_name}:',
+            validate=SpecialValidator(special_stats, stat_name),
+            default='1'
+        ).ask()
+        answers.update({stat_name: questions})
 
-        if inital_stats <= 0:
-            agility += 0
-            special_stat_loop = False
-            break
-        else:
-            while agility < 11:
-                usr_int = int(input("Agility: "))
-                if usr_int < 0 or usr_int > 10:
-                    print('Please enter a value between -1 and 11')
-                    continue
-                else:
-                    if usr_int > inital_stats and inital_stats == 0:
-                        print('Out of stat points allocating the rest as zero.')
-                        agility += 0
-                        break
-                    elif usr_int > inital_stats and inital_stats > 0:
-                        print(f'Stat points left: {inital_stats}. Please enter a value between zero and {inital_stats}.')
-                        continue
-                    else:
-                        agility += usr_int
-                        inital_stats -= usr_int
-                        break
+        special_stats[stat_name] = int(answers[stat_name])
 
-        if inital_stats <= 0:
-            luck += 0
-            special_stat_loop = False
-            break
-        else:
-            while luck < 11:
-                usr_int = int(input("Luck: "))
-                if usr_int < 0 or usr_int > 10:
-                    print('Please enter a value between -1 and 11')
-                    continue
-                else:
-                    if usr_int > inital_stats and inital_stats == 0:
-                        print('Out of stat points allocating the rest as zero.')
-                        luck += 0
-                        break
-                    elif usr_int > inital_stats and inital_stats > 0:
-                        print(f'Stat points left: {inital_stats}. Please enter a value between zero and {inital_stats}.')
-                        continue
-                    else:
-                        luck += usr_int
-                        inital_stats -= usr_int
-                        break
-        if inital_stats > 0:
-            print(f'You have unspent stat points. {inital_stats}')
-            usr_int = 0
-            continue
-        else:
-            special_stat_loop = False
-    return strength, perception, endurance, charisma, intelligence, agility, luck
+    print("\nYour chosen SPECIAL stats:")
+    for stat, value in special_stats.items():
+        print(f"{stat}: {value}")
+
+    unused_points = 40 - sum(special_stats.values())
+    print(f"\nUnused stat points: {unused_points}")
+
+    # Ask the player if they like the stats
+    like_stats = questionary.confirm("Do you like these stats?").ask()
+
+    if like_stats:
+        print("Great! Your character's SPECIAL stats are finalized.")
+    else:
+        print("Feel free to adjust your character's SPECIAL stats.")
+
+    return special_stats.values()
