0f7b4d2d42e9c08e523640fc858c3c16e13542e1
simmons2714 <67743604+simmons2714@users.noreply.github.com>
2023-07-26 21:20:57 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -30,3 +30,24 @@ def usr_choices(choice):
 
     with open(filepath, 'w') as fp:
         json.dump(information, fp, indent=4)
+
+def set_or_update_relationship(noun_name, value):
+    filepath = 'sav1.json'
+    with open(filepath, 'r') as fp:
+        information = json.load(fp)
+
+    # Check if "Relationships" key exists in the JSON data
+    relationships = information.get('Relationships', {})
+
+    # Get the current value for the NPC, or set it to 0 if it doesn't exist yet
+    current_value = relationships.get(noun_name, 0)
+
+    # Update the NPC's value in the "Relationships" dict
+    new_value = current_value + value
+    relationships[noun_name] = new_value
+
+    # Update the "Relationships" key in the main JSON data
+    information['Relationships'] = relationships
+
+    with open(filepath, 'w') as fp:
+        json.dump(information, fp, indent=4)
